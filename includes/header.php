f4f7e594e8f2771c618d133a744fb7d21f2a4fc4
snidhedinesh <snidhe2016@gmail.com>
2025-05-16 19:03:09 +0530

    new-commit

---
+++ includes/header.php
@@ -0,0 +1,27 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8" />
+  <meta name="viewport" content="width=device-width, initial-scale=1.0"/>
+  <title>Explore Your Travel</title>
+  <link rel="stylesheet" href="bootstrap/css/bootstrap.min.css">
+  <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.11.3/font/bootstrap-icons.min.css">
+  <link rel="stylesheet" href="bootstrap/style.css">
+
+ 
+</head>
+<body>
+
+
+  <div class="navbar">
+    <div class="logo">
+      <img src="bootstrap/images/Prawas_logo_round.png.png" alt="Logo" />
+    </div>
+    <div class="nav-links">
+      <a href="#">Home</a>
+      <a href="#">Group Tour Packages</a>
+      <a href="#">Car & Bus Rental</a>
+      <a href="#">About Us</a>
+      <a href="#">Contact</a>
+    </div>
+  </div>
---
+++ index.php
@@ -0,0 +1,500 @@
+<?php include 'includes/header.php' ?>
+
+
+<header>
+    <div class="header-overlay">
+        <h1>Explore Your Travel</h1>
+        <p>Discover your next great adventure, become an explorer to get started!</p>
+    </div>
+    <div class="search-bar">
+        <div>
+            <p>Find Your</p>
+            <h6>Destination</h6>
+        </div>
+        <span>
+            <p>When</p>
+
+            <select>
+                <option>When</option>
+                <option>January</option>
+                <option>February</option>
+            </select>
+        </span>
+        <span>
+            <p>Where</p>
+
+            <input type="text" placeholder="Where" />
+        </span>
+        <span>
+            <p>Where</p>
+            <select>
+
+
+                <option>What</option>
+                <option>Adventure</option>
+            </select>
+        </span>
+        <button>Search</button>
+    </div>
+</header>
+
+<section class="container-div margin-top">
+    <h2>Group Tour Packages</h2>
+    <p class="container-p">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Cras sollicitudin, tellus vitae
+        condimentum egestas, libero dolor auctor tellus, eu consectetur neque elit quis nunc. Cras elementum pretium.
+    </p>
+    <div class="carousel-container">
+        <button class="nav-btn prev-btn" onclick="scrollCards(-1)">❮</button>
+        <div class="tour-cards" id="cardContainer">
+            <!-- Repeatable Card -->
+            <div class="card">
+                <img src="bootstrap/images/tour-img-1.png" />
+                <div class="card-content">
+                    <div class="d-card-flex">
+                        <h3>Diveagar – Dapoli – Kelshi</h3>
+                        <h3 class="price">₹ 7500</h3>
+                    </div>
+                    <p>6.3 Good</p>
+                    <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Cras sollicitudin, tellus…</p>
+                    <div class="d-card-flex">
+                        <p>Every Friday (3 Days / 2 Night)</p>
+                        <p>2 Adults</p>
+                    </div>
+
+                    <div class="buttons">
+                        <button class="card-button">Trip Details</button>
+                        <button class="card-button-fill">Book</button>
+                    </div>
+                </div>
+            </div>
+
+            <div class="card">
+                <img src="bootstrap/images/tour-img-2.png" />
+                <div class="card-content">
+                    <div class="d-card-flex">
+                        <h3>Diveagar – Dapoli – Kelshi</h3>
+                        <h3 class="price">₹ 7500</h3>
+                    </div>
+                    <p>6.3 Good</p>
+                    <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Cras sollicitudin, tellus…</p>
+                    <div class="d-card-flex">
+                        <p>Every Friday (3 Days / 2 Night)</p>
+                        <p>2 Adults</p>
+                    </div>
+
+                    <div class="buttons">
+                        <button class="card-button">Trip Details</button>
+                        <button class="card-button-fill">Book</button>
+                    </div>
+                </div>
+            </div>
+
+            <div class="card">
+                <img src="bootstrap/images/tour-img-3.png" />
+                <div class="card-content">
+                    <div class="d-card-flex">
+                        <h3>Diveagar – Dapoli – Kelshi</h3>
+                        <h3 class="price">₹ 7500</h3>
+                    </div>
+                    <p>6.3 Good</p>
+                    <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Cras sollicitudin, tellus…</p>
+                    <div class="d-card-flex">
+                        <p>Every Friday (3 Days / 2 Night)</p>
+                        <p>2 Adults</p>
+                    </div>
+
+                    <div class="buttons">
+                        <button class="card-button">Trip Details</button>
+                        <button class="card-button-fill">Book</button>
+                    </div>
+                </div>
+            </div>
+            <div class="card">
+                <img src="bootstrap/images/tour-img-1.png" />
+                <div class="card-content">
+                    <div class="d-card-flex">
+                        <h3>Diveagar – Dapoli – Kelshi</h3>
+                        <h3 class="price">₹ 7500</h3>
+                    </div>
+                    <p>6.3 Good</p>
+                    <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Cras sollicitudin, tellus…</p>
+                    <div class="d-card-flex">
+                        <p>Every Friday (3 Days / 2 Night)</p>
+                        <p>2 Adults</p>
+                    </div>
+
+                    <div class="buttons">
+                        <button class="card-button">Trip Details</button>
+                        <button class="card-button-fill">Book</button>
+                    </div>
+                </div>
+            </div>
+
+            <!-- Add more cards here if needed -->
+        </div>
+        <button class="nav-btn next-btn" onclick="scrollCards(1)">❯</button>
+    </div>
+</section>
+<section class="container-div ">
+    <div class="row">
+        <div class="col-lg-6 col-md-6 col-sm-12">
+            <div class="left-img">
+                <img class="left-img-img" src="bootstrap/images/sun-img.jpg" alt="">
+                <div class="inner-img"> <img src="bootstrap/images/sun-img.jpg" alt=""></div>
+            </div>
+        </div>
+        <div class="col-lg-6 col-md-6 col-sm-12">
+            <h4 class="highlight">About</h4>
+            <h2 class="text-left">Group Tour Packages</h2>
+            <p>Welcome to Prawas Tours and Travel, your trusted companion for discovering the diverse beauty of
+                Maharashtra. From sacred pilgrimages to breathtaking tourist destinations, we offer curated tours that
+                cater to every traveler’s preferences.</p>
+            <div class="align-item"><img class="me-2" src="bootstrap/images/tick .png" alt="">
+                <p class="m-0">Wide Range of Destinations.</p>
+            </div>
+            <div class="align-item"><img class="me-2" src="bootstrap/images/tick .png" alt="">
+                <p class="m-0">Expertise and Experience.</p>
+            </div>
+            <div class="align-item"><img class="me-2" src="bootstrap/images/tick .png" alt="">
+                <p class="m-0">Exceptional Customer Service.</p>
+            </div>
+
+            <div class="count-div">
+                <div>
+                    <h1>600+</h1>
+                    <p>
+                        Happy Travelers
+                    </p>
+                </div>
+                <div>
+                    <h1>600+</h1>
+                    <p>
+                        Happy Travelers
+                    </p>
+                </div>
+                <div>
+                    <h1>600+</h1>
+                    <p>
+                        Happy Travelers
+                    </p>
+                </div>
+                <div>
+                    <h1>600+</h1>
+                    <p>
+                        Happy Travelers
+                    </p>
+                </div>
+
+            </div>
+
+        </div>
+
+    </div>
+
+</section>
+<section class="container-div gray-div margin-top">
+    <h2>Why choose Prawas Tours ?</h2>
+    <p class="container-p">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Cras sollicitudin, tellus vitae
+        condimentum egestas, libero dolor auctor tellus, eu consectetur neque elit quis nunc. Cras elementum pretium.
+    </p>
+    <div class="service-div">
+        <div class="service-inner-div">
+            <img src="bootstrap/images/calender.png" alt="">
+            <h3>Easy Booking Process</h3>
+            <p>Our user-friendly website make booking your dream vacation quick, easy, and hassle-free.</p>
+        </div>
+        <div class="service-inner-div">
+            <img src="bootstrap/images/tag.png" alt="">
+            <h3>Best Price Guarantee</h3>
+            <p>We offer competitive prices without compromising on the quality of your travel experience, ensuring you
+                get the best value for your money.</p>
+        </div>
+        <div class="service-inner-div">
+            <img src="bootstrap/images/lock.png" alt="">
+            <h3>Safety and Security</h3>
+            <p>Your safety is our top priority. We work with trusted partners and adhere to the highest safety standards
+                to ensure a worry-free journey.</p>
+        </div>
+        <div class="service-inner-div">
+            <img src="bootstrap/images/world.png" alt="">
+            <h3>Sustainable Travel Practices</h3>
+            <p>We are committed to promoting sustainable travel practices, minimizing our impact on the environment and
+                supporting local communities.</p>
+        </div>
+
+    </div>
+</section>
+<section class="container-div">
+    <div>
+        <div class="row">
+            <div class="col-lg-8 col-md-6 col-sm-12">
+                <h3>Deals & Discounts</h3>
+                <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Cras sollicitudin, tellus vitae condimentum
+                    elit
+                    egestas, libero dolor auctor tellus, eu consectetur neque elit quis.</p>
+                <div class="row">
+                    <div class="col-lg-6 col-md-6 col-sm-12">
+                        <div class="deal-div">
+                            <img src="bootstrap/images/deal.jpg" alt="">
+                            <div class="deal-inner-div">
+                              <div >
+                                <label for="">7.7 Superb</label>
+                                <p>Valley of the Kings</p>
+                              </div>
+                              <div>
+                                <p><label for="" class="strike">Rs. 9500</label>  Rs. 7500</p>
+                              </div>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="col-lg-6 col-md-6 col-sm-12">
+                        <div class="deal-div">
+                            <img src="bootstrap/images/deal.jpg" alt="">
+                            <div>
+
+                            </div>
+
+                        </div>
+                    </div>
+                </div>
+            </div>
+
+            <div class="col-lg-4 col-md-6 col-sm-12">
+                <div class="review-section">
+
+                <div class="review-card">
+    <div class="top-title">Top Reviews</div>
+
+   
+    <h3 id="reviewer-name">Dipanshu Sharma</h3>
+    <div class="stars" id="review-stars">★★★★★</div>
+    <div class="review-text" id="review-text">
+      It was a very well organised trip. Good timing, good food and very excellent Operator Kaka. The driver made our journey comfortable. Overall a very comfortable and excellent trip.
+    </div>
+    <div class="reviewer">
+      <img id="reviewer-img" src="bootstrap/images/profile.jpg" alt="Reviewer">
+      <span id="reviewer-name-footer">Dipanshu Sharma</span>
+    </div>
+  
+                </div>
+                <div class="nav-buttons">
+      <button  onclick="prevReview()"><i class="bi bi-chevron-up"></i></button>
+      <button onclick="nextReview()"><i class="bi bi-chevron-down"></i></button>
+    </div>
+            </div>
+
+        </div>
+    </div>
+</section>
+<section class="container-div gray-div">
+<h2>Popular Destinations</h2>
+    <p class="container-p">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Cras sollicitudin, tellus vitae
+        condimentum egestas, libero dolor auctor tellus, eu consectetur neque elit quis nunc. Cras elementum pretium.
+    </p>
+    <div class="carousel-container">
+        <button class="nav-btn prev-btn" onclick="scrollCardsDest(-1)">❮</button>
+        <div class="tour-cards" id="DestContainer">
+            <!-- Repeatable Card -->
+            <div class="card">
+                <img src="bootstrap/images/tour-img-1.png" />
+                <div class="card-content">
+                    <div class="d-card-flex">
+                        <h3>Diveagar – Dapoli – Kelshi</h3>
+                        <h3 class="price">₹ 7500</h3>
+                    </div>
+                    <p>6.3 Good</p>
+                    <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Cras sollicitudin, tellus…</p>
+                    <div class="d-card-flex">
+                        <p>Every Friday (3 Days / 2 Night)</p>
+                        <p>2 Adults</p>
+                    </div>
+
+                    
+                </div>
+            </div>
+
+            <div class="card">
+                <img src="bootstrap/images/tour-img-2.png" />
+                <div class="card-content">
+                    <div class="d-card-flex">
+                        <h3>Diveagar – Dapoli – Kelshi</h3>
+                        <h3 class="price">₹ 7500</h3>
+                    </div>
+                    <p>6.3 Good</p>
+                    <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Cras sollicitudin, tellus…</p>
+                    <div class="d-card-flex">
+                        <p>Every Friday (3 Days / 2 Night)</p>
+                        <p>2 Adults</p>
+                    </div>
+
+                   
+                </div>
+            </div>
+
+            <div class="card">
+                <img src="bootstrap/images/tour-img-3.png" />
+                <div class="card-content">
+                    <div class="d-card-flex">
+                        <h3>Diveagar – Dapoli – Kelshi</h3>
+                        <h3 class="price">₹ 7500</h3>
+                    </div>
+                    <p>6.3 Good</p>
+                    <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Cras sollicitudin, tellus…</p>
+                    <div class="d-card-flex">
+                        <p>Every Friday (3 Days / 2 Night)</p>
+                        <p>2 Adults</p>
+                    </div>
+
+                   
+                </div>
+            </div>
+            <div class="card">
+                <img src="bootstrap/images/tour-img-1.png" />
+                <div class="card-content">
+                    <div class="d-card-flex">
+                        <h3>Diveagar – Dapoli – Kelshi</h3>
+                        <h3 class="price">₹ 7500</h3>
+                    </div>
+                    <p>6.3 Good</p>
+                    <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Cras sollicitudin, tellus…</p>
+                    <div class="d-card-flex">
+                        <p>Every Friday (3 Days / 2 Night)</p>
+                        <p>2 Adults</p>
+                    </div>
+
+                </div>
+            </div>
+
+            <!-- Add more cards here if needed -->
+        </div>
+        <button class="nav-btn next-btn" onclick="scrollCardsDest(1)">❯</button>
+    </div>
+
+  
+<p class="container-p">Lorem ipsum dolor sit amet consectetur adipisicing elit. Facilis laboriosam porro sequi provident quae, accusamus ea voluptates eligendi dolorem cumque iusto qui quod odio voluptas aliquam adipisci, est vitae. Enim!</p>
+<div class="text-center">                        <button class="container-button">Book</button>
+</div>
+</section>
+<section>
+  <img class="video" src="bootstrap/images/video.jpg" alt="">
+</section>
+<section class="container-div">
+<h2>Interesting Facts</h2>
+    <p class="container-p">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Cras sollicitudin, tellus vitae
+        condimentum egestas, libero dolor auctor tellus, eu consectetur neque elit quis nunc. Cras elementum pretium.
+        <div class="fact-container">
+        <button class="nav-btn prev-btn" onclick="scrollfactDest(-1)">❮</button>
+
+        <div class="fact-div" id="fact-div">
+
+  <div>
+    <img src="bootstrap/images/icon-home-1.png" alt="">
+  </div>
+  <div>
+    <img src="bootstrap/images/icon-home-2.png" alt="">
+  </div>
+  <div>
+    <img src="bootstrap/images/icon-home-3.png" alt="">
+  </div>
+  <div>
+    <img src="bootstrap/images/icon-home-4.png" alt="">
+  </div>
+
+
+<div>
+    <img src="bootstrap/images/icon-home-5.png" alt="">
+  </div>
+  <div>
+    <img src="bootstrap/images/icon-home-6.png" alt="">
+  </div>
+  <div>
+    <img src="bootstrap/images/icon-home-7.png" alt="">
+  </div>
+  <div>
+    <img src="bootstrap/images/icon-home-1.png" alt="">
+  </div>
+  <div>
+    <img src="bootstrap/images/icon-home-2.png" alt="">
+  </div>
+  <div>
+    <img src="bootstrap/images/icon-home-3.png" alt="">
+  </div>
+  <button class="nav-btn next-btn" onclick="scrollfactDest(1)">❯</button>
+  </div>
+</section>
+
+
+
+<!-- JS for Scroll -->
+<script>
+     function scrollCardsDest(direction) {
+        const container = document.getElementById("DestContainer");
+        const scrollAmount = 330; // adjust based on card width
+        container.scrollBy({
+            left: direction * scrollAmount,
+            behavior: "smooth"
+        });
+    }
+    function scrollfactDest(direction) {
+        const container = document.getElementById("fact-div");
+        const scrollAmount = 150; // adjust based on card width
+        container.scrollBy({
+            left: direction * scrollAmount,
+            behavior: "smooth"
+        });
+    }
+    function scrollCards(direction) {
+        const container = document.getElementById("cardContainer");
+        const scrollAmount = 330; // adjust based on card width
+        container.scrollBy({
+            left: direction * scrollAmount,
+            behavior: "smooth"
+        });
+    }
+    const reviews = [
+      {
+        name: "Dipanshu Sharma",
+        star: "★★★★★",
+        text: "It was a very well organised trip. Good timing, good food and very excellent Operator Kaka. The driver made our journey comfortable. Overall a very comfortable and excellent trip.",
+        image: "bootstrap/images/profile.jpg"
+      },
+      {
+        name: "Anita Verma",
+        star: "★★★★",
+        text: "Amazing service and coordination! The staff was friendly and everything was perfectly planned.",
+        image: "bootstrap/images/profile.jpg"
+      },
+      {
+        name: "Rahul Nair",
+        star: "★★★",
+        text: "A seamless experience from booking to journey. Highly recommended for anyone looking for a hassle-free trip.",
+        image: "bootstrap/images/profile.jpg"
+      }
+    ];
+
+    let current = 0;
+
+    function updateReview() {
+      document.getElementById("reviewer-name").innerText = reviews[current].name;
+      document.getElementById("review-text").innerText = reviews[current].text;
+      document.getElementById("reviewer-name-footer").innerText = reviews[current].name;
+      document.getElementById("reviewer-img").src = reviews[current].image;
+      document.getElementById("review-stars").innerText = reviews[current].star;
+
+    }
+
+    function nextReview() {
+      current = (current + 1) % reviews.length;
+      updateReview();
+    }
+
+    function prevReview() {
+      current = (current - 1 + reviews.length) % reviews.length;
+      updateReview();
+    }
+  
+</script>
+
+</body>
+
+</html>
